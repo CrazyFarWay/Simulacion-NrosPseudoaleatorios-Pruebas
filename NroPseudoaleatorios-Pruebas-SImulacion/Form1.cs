8b55c8d79fb5e6d9a365921483f7aff8bb4ae72a
crazyfarway <piano201219@gmail.com>
2023-05-09 13:26:37 -0300

    Add solution

---
+++ NroPseudoaleatorios-Pruebas-SImulacion/Form1.cs
@@ -0,0 +1,10 @@
+namespace NroPseudoaleatorios_Pruebas_SImulacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
