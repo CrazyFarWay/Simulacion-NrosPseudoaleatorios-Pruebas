37bc0f4764589d41b8c1006268d376f8bf552cbe
crazyfarway <piano201219@gmail.com>
2023-05-09 20:48:52 -0300

    Solucionar metodo de generar por parte central del cuadrado y agregar UI correspondiente

---
+++ NroPseudoaleatorios-Pruebas-SImulacion/Controller.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NroPseudoaleatorios_Pruebas_SImulacion
+{
+    internal class Controller
+    {
+        public double[] calcularParteCentralCuadrado(Int64 digitosDeseados, Int64 semilla)
+        {
+            List<double> arrayResultados = new List<double>();
+
+            // Generar los primeros 10 números pseudoaleatorios
+            for (int i = 0; i < digitosDeseados; i++)
+            {
+                // Elevar la semilla al cuadrado
+                Int64 cuadrado = semilla * semilla;
+                string cuadradoStr = cuadrado.ToString();
+                int cantidadDigitosCuadrado = cuadradoStr.Length;
+
+                if ( (cantidadDigitosCuadrado - digitosDeseados) % 2 != 0 )
+                {
+                    cuadrado = cuadrado * 10;
+                }
+
+                // Tomar los dígitos centrales del cuadrado como la nueva semilla
+                int digitos = Convert.ToInt32(digitosDeseados);
+
+                int centro = cuadradoStr.Length / 2;
+                Int64 nuevoNum = Int64.Parse(cuadradoStr.Substring(centro - (digitos / 2), digitos));
+
+                // Asignar el nuevo número como la semilla para la próxima iteración
+                semilla = nuevoNum;
+                int cantidadDigitosSemilla = semilla.ToString().Length;
+                double nuevoNumDecimal = (double) semilla / (double) Math.Pow(10, cantidadDigitosSemilla);
+                arrayResultados.Add(nuevoNumDecimal);
+
+
+                // Mostrar el número pseudoaleatorio generado
+                Console.WriteLine(nuevoNumDecimal);
+            }
+
+            return arrayResultados.ToArray();
+        }
+
+        public void calcularLehmer()
+        {
+
+        }
+
+        public void calcularCongruencialMixto()
+        {
+
+        }
+
+        public void calcularCongruencialMultiplicativo()
+        {
+
+        }
+
+        public void calcularCongruencialAditivo()
+        {
+
+        }
+    }
+}
--- NroPseudoaleatorios-Pruebas-SImulacion/Form1.cs
+++ NroPseudoaleatorios-Pruebas-SImulacion/Form1.cs
@@ -1,10 +1,26 @@
+using System.Windows.Forms;
+
 namespace NroPseudoaleatorios_Pruebas_SImulacion
 {
     public partial class Form1 : Form
     {
+        Controller controller = new Controller();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void botonCalcular_Click(object sender, EventArgs e)
+        {
+            int digitosDeseados = int.Parse(digitosDeseadosTextBox.Text);
+            int semilla = int.Parse(semillaTextBox.Text);
+            double[] resultados = controller.calcularParteCentralCuadrado(digitosDeseados, semilla);
+            resultRichTextBox.Text = "";
+
+            for (int i = 0; i < resultados.Length; i++)
+            {
+                resultRichTextBox.AppendText(resultados[i].ToString() + "\n");
+            }
         }
     }
 }
