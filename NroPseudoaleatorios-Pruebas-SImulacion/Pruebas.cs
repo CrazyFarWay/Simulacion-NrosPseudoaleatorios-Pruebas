65974ce322de4993ea37e323b06d70d701d4b609
crazyfarway <piano201219@gmail.com>
2023-05-12 21:23:46 -0300

    Agregar pruebas

--- NroPseudoaleatorios-Pruebas-SImulacion/Pruebas.cs
+++ NroPseudoaleatorios-Pruebas-SImulacion/Pruebas.cs
@@ -8,290 +8,169 @@ namespace NroPseudoaleatorios_Pruebas_SImulacion
 {
     internal class Pruebas
     {
-        static void Menu(string[] args)
+        public static bool PruebaPromedio(double[] numerosPseudoaleatorios, double estadisticoAlfa)
         {
-            int opcion;
-            do
-            {
-                Console.WriteLine("Seleccione un método para ejecutar:");
-                Console.WriteLine("1. Prueba de promedio");
-                Console.WriteLine("2. Prueba de frecuencias");
-                Console.WriteLine("3. Prueba de serie");
-                Console.WriteLine("4. Prueba de Kolmogorov-Smirnov");
-                Console.WriteLine("5. Prueba de corrida arriba-abajo-media");
-                Console.WriteLine("0. Salir");
-
-                opcion = Convert.ToInt32(Console.ReadLine());
-
-                switch (opcion)
-                {
-                    case 1:
-                        // Llamar al método PruebaPromedio con los parámetros necesarios
-                        Console.WriteLine("Ingrese el valor de n:");
-                        int n = Convert.ToInt32(Console.ReadLine());
-                        double[] datos = new double[n];
-                        Console.WriteLine("Ingrese los datos:");
-                        for (int i = 0; i < n; i++)
-                        {
-                            datos[i] = Convert.ToDouble(Console.ReadLine());
-                        }
-                        Console.WriteLine("Ingrese el valor de Zc:");
-                        double Zc = Convert.ToDouble(Console.ReadLine());
-                        PruebaPromedio(n, datos, Zc);
-                        break;
-                    case 2:
-                        // Llamar al método PruebaFrecuencias con los parámetros necesarios
-                        Console.WriteLine("Ingrese el valor de n:");
-                        int n2 = Convert.ToInt32(Console.ReadLine());
-                        double[] datos2 = new double[n2];
-                        Console.WriteLine("Ingrese los datos:");
-                        for (int i = 0; i < n2; i++)
-                        {
-                            datos2[i] = Convert.ToDouble(Console.ReadLine());
-                        }
-                        Console.WriteLine("Ingrese el valor de x:");
-                        int x = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Ingrese el valor de alfa:");
-                        double alfa = Convert.ToDouble(Console.ReadLine());
-                        PruebaFrecuencias(n2, datos2, x, alfa);
-                        break;
-                    case 3:
-                        Console.WriteLine("Ingrese los números separados por coma:");
-                        string numerosStr = Console.ReadLine();
-                        double[] numeros = Array.ConvertAll(numerosStr.Split(','), Double.Parse);
-
-                        Console.WriteLine("Ingrese el valor de x:");
-                        int x = Convert.ToInt32(Console.ReadLine());
-
-                        bool resultadoSerie = PruebaSerie(numeros, x);
-                        Console.WriteLine(resultadoSerie ? "Se acepta la hipótesis" : "Se rechaza la hipótesis");
-                        break;
-                    case 4:
-                        Console.WriteLine("Ingrese el valor de n:");
-                        int n4 = Convert.ToInt32(Console.ReadLine());
-                        double[] datos4 = new double[n4];
-                        Console.WriteLine("Ingrese los datos:");
-                        for (int i = 0; i < n4; i++)
-                        {
-                            datos4[i] = Convert.ToDouble(Console.ReadLine());
-                        }
-                        Console.WriteLine("Ingrese el nivel de significancia:");
-                        double nivelSignificancia = Convert.ToDouble(Console.ReadLine());
-                        bool resultadoKS = PruebaKolmogorovSmirnov(datos4, nivelSignificancia);
-                        Console.WriteLine(resultadoKS ? "Se acepta la hipótesis" : "Se rechaza la hipótesis");
-                        break;
-                    case 5:
-                        Console.WriteLine("Ingrese el valor de n:");
-                        int n5 = Convert.ToInt32(Console.ReadLine());
-                        List<double> numeros5 = new List<double>();
-                        Console.WriteLine("Ingrese los datos:");
-                        for (int i = 0; i < n5; i++)
-                        {
-                            numeros5.Add(Convert.ToDouble(Console.ReadLine()));
-                        }
-                        bool resultadoCorrida = PruebaCorridaArribaAbajoMedia(numeros5);
-                        Console.WriteLine(resultadoCorrida ? "Se acepta la hipótesis" : "Se rechaza la hipótesis");
-                        break;
-                    case 0:
-                        Console.WriteLine("Saliendo del programa...");
-                        break;
-                    default:
-                        Console.WriteLine("Opción inválida. Seleccione una opción válida.");
-                        break;
-                }
-
-            } while (opcion != 0);
-        }
-        public static bool PruebaPromedio(int n, double[] datos, double Zc)
-        {
+            int cantidadNumeros = numerosPseudoaleatorios.Length;
             // Calcular el promedio aritmético de los datos
-            double promedio = 0;
-            for (int i = 0; i < n; i++)
-            {
-                promedio += datos[i];
-            }
-            promedio /= n;
+            double promedio = numerosPseudoaleatorios.Average();
 
             // Calcular el estadístico Z
-            double Z = (promedio - 0.5) * Math.Sqrt(n) / Math.Sqrt(1.0 / 12.0);
+            double estadistico = ((promedio - 0.5) * Math.Sqrt(cantidadNumeros)) / (Math.Sqrt(1.0 / 12.0));
 
             // Verificar si se cumple la hipótesis
-            if (Math.Abs(Z) < Zc)
-            {
-                Console.WriteLine("No se puede rechazar la hipótesis de que los números pseudo aleatorios generados provienen de un universo uniforme con media 0.5.");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Se rechaza la hipótesis de que los números pseudo aleatorios generados provienen de un universo uniforme con media 0.5.");
-                return false;
-            }
+            return Math.Abs(estadistico) < estadisticoAlfa;
         }
-            public static bool PruebaFrecuencias(int n, double[] datos, int x, double alfa)
-            {
-                // Calcular la frecuencia esperada en cada subintervalo
-                double[] frecuenciaEsperada = new double[x];
-                for (int i = 0; i < x; i++)
-                {
-                    frecuenciaEsperada[i] = (double)n / x;
-                }
 
-                // Calcular la frecuencia observada en cada subintervalo
-                double[] frecuenciaObservada = new double[x];
-                for (int i = 0; i < n; i++)
-                {
-                    int j = (int)(datos[i] * x);
-                    frecuenciaObservada[j]++;
-                }
-
-                // Calcular el estadístico chi-cuadrado
-                double chiCuadrado = 0;
-                for (int i = 0; i < x; i++)
-                {
-                    double diferencia = frecuenciaObservada[i] - frecuenciaEsperada[i];
-                    chiCuadrado += diferencia * diferencia / frecuenciaEsperada[i];
-                }
-
-                // Calcular el valor crítico
-                double valorCritico = ChiCuadrado.InvCDF(x - 1, 1 - alfa);
-
-                // Verificar si se cumple la hipótesis
-                if (chiCuadrado < valorCritico)
-                {
-                    Console.WriteLine("No se puede rechazar la hipótesis de que los números pseudo aleatorios generados provienen de un universo uniforme.");
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("Se rechaza la hipótesis de que los números pseudo aleatorios generados provienen de un universo uniforme.");
-                    return false;
-                }
-            }
-
-        public static bool PruebaSerie(double[] numeros, int x)
+        public static bool PruebaFrecuencias(double[] numerosPseudoaleatorios, int numeroSubintervalos, double estadisticoAlfa)
         {
-            int n = numeros.Length;
-            int x2 = x * x;
-            int[] frecuenciasObservadas = new int[x2];
+            int cantidadNumeros = numerosPseudoaleatorios.Length;
 
-            // Dividir el cuadrado unitario en x2 celdas
-            for (int i = 0; i < n - 1; i += 2)
+            // Calcular la frecuencia esperada en cada subintervalo
+            double frecuenciaEsperada = (double) cantidadNumeros / numeroSubintervalos;
+
+            // Calcular la frecuencia observada en cada subintervalo
+            double[] frecuenciaObservada = new double[numeroSubintervalos];
+
+            for (int i = 0; i < cantidadNumeros; i++)
             {
-                int j = (int)(numeros[i] * x);
-                int k = (int)(numeros[i + 1] * x);
-                frecuenciasObservadas[j * x + k]++;
+                int subintervaloCorrespondiente = (int)(numerosPseudoaleatorios[i] * numeroSubintervalos);
+                frecuenciaObservada[subintervaloCorrespondiente]++;
             }
+
+            // Calcular el estadístico chi-cuadrado
+            double chiCuadrado = 0;
+            for (int i = 0; i < numeroSubintervalos; i++)
+            {
+                double diferenciaFrecuencias = frecuenciaObservada[i] - frecuenciaEsperada;
+                chiCuadrado += diferenciaFrecuencias * diferenciaFrecuencias;
+            }
+
+            chiCuadrado /= frecuenciaEsperada;
+
+            // Verificar si se cumple la hipótesis
+            return chiCuadrado < estadisticoAlfa;
+        }
+
+        public static bool PruebaSerie(List<(double, double)> tuplasNumerosPseudoaleatorios, int x, double estadisticoAlfa)
+        {
+            int cantidadTuplas = tuplasNumerosPseudoaleatorios.Count;
+            int cantidadCeldas = x * x;
+
+            // Calcular frecuencia observada en cada celda
+            int[,] frecuenciasObservadas = new int[x, x];
+
+            for (int numeroTupla = 0; numeroTupla < cantidadTuplas; numeroTupla++)
+            {
+                (double, double) tupla = tuplasNumerosPseudoaleatorios[numeroTupla];
+                 
+                int j = (int) (tupla.Item1 * x);
+                int k = (int) (tupla.Item2 * x);
+                frecuenciasObservadas[j, k]++;
+            }
+
+            // Calcular frecuencia esperada
+            double frecuenciaEsperada = (double) cantidadTuplas / cantidadCeldas;
 
             // Calcular estadístico
-            double frecuenciaEsperada = (double)n / x2;
-            double estadistico = 0.0;
+            double chiCuadrado = 0.0;
             for (int j = 0; j < x; j++)
             {
                 for (int k = 0; k < x; k++)
                 {
-                    double fjK = frecuenciasObservadas[j * x + k];
-                    estadistico += (fjK - frecuenciaEsperada) * (fjK - frecuenciaEsperada);
+                    double diferenciaFrecuencias = frecuenciasObservadas[j, k] - frecuenciaEsperada;
+                    chiCuadrado += diferenciaFrecuencias * diferenciaFrecuencias;
                 }
             }
-            estadistico *= x2 / (double)n;
+            chiCuadrado *= (double) cantidadCeldas / cantidadTuplas;
 
             // Comparar estadístico con chi-cuadrado
-            int gradosLibertad = x2 - x;
-            double chiCuadrado = DistribucionChiCuadrado.CalcularValor(gradosLibertad, 0.05);
-            return estadistico < chiCuadrado;
+            return chiCuadrado < estadisticoAlfa;
         }
 
-        public bool PruebaKolmogorovSmirnov(double[] numerosAleatorios, double nivelSignificancia)
+        public bool PruebaKolmogorovSmirnov(double[] numerosPseudoaleatorios, double estadisticoAlfa)
         {
-            int n = numerosAleatorios.Length;
-            double[] x = numerosAleatorios.OrderBy(num => num).ToArray();
-            double[] En = new double[n];
-            double Dn = 0.0;
+            int cantidadNumeros = numerosPseudoaleatorios.Length;
+         
+            // Ordenar lista de numeros
+            double[] numerosOrdenados = numerosPseudoaleatorios.OrderBy(num => num).ToArray();
 
-            for (int i = 0; i < n; i++)
+            // Calcular distribuciones acumuladas
+            double[] distribucionesAcumuladas = new double[cantidadNumeros];
+
+            for (int i = 0; i < cantidadNumeros; i++)
             {
-                En[i] = (double)(i + 1) / n;
+                distribucionesAcumuladas[i] = (double) (i + 1) / cantidadNumeros;
             }
 
-            for (int i = 0; i < n; i++)
+            // Calcular estadistico k-s
+            double[] diferenciasFrecuenciaNumero = new double[cantidadNumeros];
+            double estadisticoKs = 0.0;
+
+            for (int i = 0; i < cantidadNumeros; i++)
             {
-                double diff1 = Math.Abs(En[i] - x[i]);
-                double diff2 = Math.Abs(En[i] - x[i + 1]);
-                double maxDiff = Math.Max(diff1, diff2);
-                Dn = Math.Max(Dn, maxDiff);
+                diferenciasFrecuenciaNumero[i] = Math.Abs(distribucionesAcumuladas[i] - numerosOrdenados[i]);
             }
 
-            double da = 1.36 / Math.Sqrt(n);
-            return Dn < da;
+            estadisticoKs = diferenciasFrecuenciaNumero.Max();
+
+            return estadisticoKs < estadisticoAlfa;
         }
 
-        public static bool PruebaCorridaArribaAbajoMedia(List<double> numeros)
+        public static bool PruebaCorridaArribaAbajoMedia(double[] numerosPseudoaleatorios, double estadisticoAlfa)
         {
-            int n = numeros.Count;
-            double media = numeros.Average();
-            int[] s = new int[n]; // secuencia binaria
-            int[] r = new int[n]; // secuencia de corridas
-            int frecuenciaEsperada;
-            int frecuenciaObservada;
+            int cantidadNumeros = numerosPseudoaleatorios.Length;
 
             // Generar la secuencia binaria
-            for (int i = 0; i < n; i++)
+            int[] secuenciaBinaria = new int[cantidadNumeros];
+
+            for (int i = 0; i < cantidadNumeros; i++)
             {
-                if (numeros[i] < media)
-                {
-                    s[i] = 0;
-                }
-                else
-                {
-                    s[i] = 1;
-                }
+                secuenciaBinaria[i] = (numerosPseudoaleatorios[i] <= 0.5) ? 0 : 1;
             }
 
             // Calcular la secuencia de corridas
-            int j = 0;
-            int k = 0;
-            for (int i = 0; i < n; i++)
+            List<int> frecuenciasObservadas = new List<int>();
+
+            for (int i = 0; i < secuenciaBinaria.Length; i++)
             {
-                if (i == 0)
+                int contador = 1;
+                int numeroBinario = secuenciaBinaria[i];
+
+                for (int j = i; j < secuenciaBinaria.Length - i; j++)
                 {
-                    r[j] = s[i];
-                    k = s[i];
-                }
-                else
-                {
-                    if (s[i] == k)
+                   int otroNumeroBinario = secuenciaBinaria[j];
+
+                   if (numeroBinario == otroNumeroBinario)
                     {
-                        r[j]++;
-                    }
+                        contador++;
+                    } 
                     else
                     {
-                        k = s[i];
-                        j++;
-                        r[j] = 1;
+                        break;
                     }
                 }
-            }
-            int totalCorridas = j + 1;
 
-            // Calcular la frecuencia esperada y observada
-            double chiCuadrado = 0.0;
-            for (int i = 1; i <= n; i++)
+                frecuenciasObservadas.Add(contador);
+            }
+
+            int cantidadFrecuenciasObservadas = frecuenciasObservadas.Count;
+            double[] frecuenciasEsperadas = new double[cantidadFrecuenciasObservadas];
+
+            for (int i = 0; i < cantidadFrecuenciasObservadas; i++)
             {
-                if (i <= (n + 1) / 2)
-                {
-                    frecuenciaEsperada = (n - i + 3) / Math.Pow(2, i + 1);
-                }
-                else
-                {
-                    frecuenciaEsperada = (n - i + 3) / Math.Pow(2, n - i + 1);
-                }
-                frecuenciaObservada = r[i - 1];
-                chiCuadrado += Math.Pow(frecuenciaObservada - frecuenciaEsperada, 2) / frecuenciaEsperada;
+                frecuenciasEsperadas[i] = (cantidadNumeros - i + 3) / (Math.Pow(2, i + 1));
             }
 
-            // Comparar con el valor crítico
-            double valorCritico = ChiCuadrado.InvCDF((totalCorridas - 1), 0.05);
-            return chiCuadrado < valorCritico;
+            // Calcular chi cuadrado
+            double chiCuadrado = 0.0;
+            for (int i = 1; i <= cantidadFrecuenciasObservadas; i++)
+            {
+                double diferenciaFrecuencias = (frecuenciasObservadas[i] - frecuenciasEsperadas[i]);
+                chiCuadrado += (diferenciaFrecuencias * diferenciaFrecuencias) / frecuenciasEsperadas[i];
+            }
+
+            // Comparar estadístico con chi-cuadrado
+            return chiCuadrado < estadisticoAlfa;
         }
 
     }
